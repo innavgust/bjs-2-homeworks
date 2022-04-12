9827e2c6323b001f29286ca876e1ffd777be710d
innavgust <innavgust@gmail.com>
2022-04-12 13:29:17 +0300

    second version

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -3,55 +3,56 @@
 function parseCount(num) {
   let count = Number.parseInt(num);
   if (isNaN(count)) {
-    const countError = new Error("Невалидное значение");
-    throw countError;
+    throw new Error("Невалидное значение");
+  } else {
+    return count;
   }
-  return count;
 }
 
 function validateCount(num) {
   try {
     let finalCount = parseCount(num);
     return finalCount;
-  }catch(e) {
-    console.log('Ошибка произошла!');
+  } catch(e) {
+    console.log(e.message);
   }
 }
 
-let res = validateCount(5);
-console.log (res);
+let res = validateCount();
+console.log(res);
+
+console.log();
 //задача 2
 
 class Triangle {
-    constructor(firstSide, secondSide, thirdSide) {
-      this.firstSide = firstSide;
-      this.secondSide = secondSide;
-      this.thirdSide = thirdSide;
-      if ((firstSide + secondSide <= thirdSide) || (firstSide + thirdSide <= secondSide) || (secondSide + thirdSide <= firstSide)) {
-        const triangleError = new Error("Треугольник с такими сторонами не существует");
-        throw triangleError;
-      }
-    }
-  
-    getPerimeter() {
-      return (this.firstSide + this.secondSide + this.thirdSide);
-    }
-  
-    getArea() {
-      let p = (this.firstSide + this.secondSide + this.thirdSide) / 2;
-      let area = Math.sqrt(p*(p - this.firstSide)*(p - this.secondSide)*(p - this.thirdSide));
-      return Number(area.toFixed(3));
+  constructor(firstSide, secondSide, thirdSide) {
+    this.firstSide = firstSide;
+    this.secondSide = secondSide;
+    this.thirdSide = thirdSide;
+    if ((firstSide + secondSide <= thirdSide) || (firstSide + thirdSide <= secondSide) || (secondSide + thirdSide <= firstSide)) {
+      throw new Error("Треугольник с такими сторонами не существует");
     }
   }
-  
-  function getTriangle(a, b, c) {
+
+  getPerimeter() {
+    return (this.firstSide + this.secondSide + this.thirdSide);
+  }
+
+  getArea() {
+    let p = (this.getPerimeter()) / 2;
+    let area = Math.sqrt(p*(p - this.firstSide)*(p - this.secondSide)*(p - this.thirdSide));
+    return Number(area.toFixed(3));
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
     let triangle1 = new Triangle(a, b, c);
-    try {
-      return {firstSide: firstSide, secondSide: secondSide, thirdSide: thirdSide};
-    }catch(e) {
-      console.log('Ошибка! Треугольник не существует!');
-    }
+    return triangle1;
+  }catch(err) {
+    console.log('Ошибка! Треугольник не существует');
   }
-  
-  let result = getTriangle(2, 5, 5);
-  console.log (result);
+}
+
+let result = getTriangle(11, 5, 4);
+console.log (result);
